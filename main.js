32e4f527e4e4e403fbd835480b73ec6f4ae2b2b4
MPR <info@dm-development.de>
2016-12-29 17:35:09 +0100

    delete var
    back botton

--- main.js
+++ main.js
@@ -8,18 +8,17 @@ or substantial portions of the Software.
 */
 
 
-
 var follers = new Array(); // Array of followers
 var ing = 0; // Array pointer - username to check
-var j = 0; // counter
-var followercnt = 0; // Counter of (your) followers
-var followingcnt = 0; // Counter (DJs you are) following
-var cnt = 0;  //counter
-var offst = 0; // for paging of content 
-var lmt = 100; //  limit 100 for 1 page 
-var follok = 0; //number of followers that are following back
-var follnot = 0; //number of followers that are NOT following back
-
+var j; // counter of follower (Frontend: Data Calculated)
+var w; // counter of following (Frontend: Data Calculated)
+var followercnt; // Counter of (your) followers
+var followingcnt; // Counter (DJs you are) following
+var cnt;  //counter
+var offst = 0; // for paging of content
+var lmt = 100; //  limit 100 for 1 page
+var follok = 0; //number of followers that are following back (green)
+var follnot = 0; //number of followers that are NOT following back (green)
 
 
 $(document).ready(function () {
@@ -61,23 +60,18 @@ $.getJSON('http://api.mixcloud.com/' + uname + '/?metadata=1', function (result)
 
             // Show data
             $("#info").append('Followers:' + followercnt + ' <br />Following:' + followingcnt + '');
-  //  console.log(followercnt + " - " + followingcnt)
-
 
             // how often get 100x followers
             if (followercnt == "0") {
                 $("#error").append('User was not found on Mixcloud<br /><br /><br /><br />');}
             var tempcnt = (followercnt / 100);
             var cnt = Math.ceil(tempcnt);
-  //  console.log("q." + cnt + " - " + tempcnt + " - " + followercnt);
 
             // how often get 100x following
             if (followingcnt == "0") {
             $("#error").append('User was not found on Mixcloud<br /><br /><br /><br />'); }
             var tempcntb = (followingcnt / 100);
             var cntb = Math.ceil(tempcntb);
-  //  console.log("q2." + cntb + " - " + tempcntb + " - " + followingcnt);
-
 
 
 // ************* START *************
@@ -92,7 +86,6 @@ getfollowing(cntb, uname);
 
 // Get list of followers +++++++++++++++++++++++++
 function getfolls(cnt, uname) {
-                  console.log("x." + uname + " -x " + cnt + " -x " + followercnt);
 
                   for (var i = 0; i < cnt; i++) {
                           $.getJSON('http://api.mixcloud.com/' + uname + '/followers/?limit=' + lmt + '&offset=' + offst + '', function (result) {
@@ -101,22 +94,19 @@ function getfolls(cnt, uname) {
                                      var temp = (result.data[r].username);
                                      follers[j] = temp;
                                      j++;
-              //     console.log("getfolls." + uname + " - " + r + " -x " + followercnt);
                                      $("#cntfollowers").text('Followers:' + j + ' ');
                                };
                              });
                            offst = (offst + 100);
-              //     console.log("b." + j + " = ");
                  };
   return follers
 }
 
 // Get list of ppl you follow +++++++++++++++++++++++++
-
 function getfollowing(cntb, uname) {
         var offstb = 0;
         var lmtb = 100;
-        var w = 0;
+        var w = 0; // counter of following (Frontend: Data Calculated)
 
         for (var i = 0; i < cntb; i++) {
 
@@ -127,12 +117,9 @@ function getfollowing(cntb, uname) {
                     var realname = result.data[r].name;
                     w++;
                     $("#cntfollowing").text(' Following:' + w + '');
-  // console.log("c." + w + " = " +  offstb  + " - " + r + " - " + followingcnt + "");
                     var tr = follers.indexOf(ing);
- //  console.log("c." + j + " = " + r + " = " + followercnt);
                     if (tr != -1) {
                          follok = follok + 1;
-//  console.log("aaa." + follok + " - " + follok);
                          $("#fok").html('<div>' + follok + ' </div>');
                          $("#followingok").append('<div><img src="./pictures/ok.png" height="12" width="12"> ' + ing + ' <a target=\'_blank\' href=\'http://mixcloud.com/' + ing + '\'>open profile</a></div>');
                     } else {
